a7faa7f30efb48a0cec52cc83ff6a12df8a6632d
krishnapal <krishnapalkp05@gmail.com>
2026-01-08 12:27:31 +0000

    created script

---
+++ scripts/sync-folder-a.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+TARGET_REPO="repo-A"
+TARGET_ORG="kpsingh21"   # same account
+TARGET_BRANCH="main"
+
+echo "Syncing folder-a to $TARGET_REPO..."
+
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Actions"
+
+git clone https://github.com/$TARGET_ORG/$TARGET_REPO.git
+rm -rf $TARGET_REPO/*
+cp -r folder-a/* $TARGET_REPO/
+
+cd $TARGET_REPO
+
+git add .
+git commit -m "Sync folder-a from source repo" || echo "No changes"
+git push origin $TARGET_BRANCH
